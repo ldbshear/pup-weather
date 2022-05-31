ac18260ccfcec7dfc0f1280e6e66c1a396cea270
Ldbshear-jones <ldbshearjones@gmail.com>
2022-05-31 00:09:38 -0400

    adding container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
-import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className="App">
       <h1>Testing</h1>
-      <Button variant="primary">Primary</Button>
+      <Container className="p-3">
+        <Container className="p-5 mb-4 bg-light rounded-3">
+          <h1 className="header">Welcome To React-Bootstrap</h1>
+          <div>
+            We now have Toasts
+            <span role="img" aria-label="tada">
+              🎉
+            </span>
+          </div>
+        </Container>
+      </Container>
     </div>
   );
 }
