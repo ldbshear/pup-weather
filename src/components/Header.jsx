3be1f06bb8ec8524381df63ef410c2f4ba021cd9
Ldbshear-jones <ldbshearjones@gmail.com>
2022-05-30 22:52:50 -0400

    added header navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Header() {
+  return (
+    <>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>Weather Pup</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
