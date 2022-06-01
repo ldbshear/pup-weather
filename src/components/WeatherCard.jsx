001d872dd99a22a3aea8e6dd79fcce0550210b26
Ldbshear-jones <ldbshearjones@gmail.com>
2022-06-01 12:57:44 -0400

    adding api call

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
@@ -9,18 +10,65 @@ import Row from "react-bootstrap/Row";
 import CurrentWeather from "./CurrentWeather";
 
 export default function WeatherCard() {
+  const [userCity, showUserCity] = useState("");
+  const [location, showLocation] = useState("");
+  const [weatherConditions, getWeatherConditions] = useState({
+    temp: "",
+    weatherDesc: "",
+  });
+
+  function getWeather(response) {
+    getWeatherConditions({
+      temp: response.data.main.temp,
+      weatherDesc: response.data.weather[0].main,
+    });
+  }
+
+  function updateLocation(event) {
+    showUserCity(event.target.value);
+  }
+  function requestData() {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4bcd69482e48eff55262cb4ddf9efad9&units=metric`;
+
+    axios
+      .get(url)
+      .then(getWeather)
+      .catch(function (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  }
+
   return (
     <>
-      <Form className="">
+      <Form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log("submitted");
+          showLocation(userCity);
+        }}
+        className=""
+      >
         <InputGroup className="mb-3 w-50 mx-auto">
           <FormControl
             size="lg"
             type="text"
             placeholder="Enter city"
+            onChange={(event) => {
+              updateLocation(event);
+            }}
+            value={userCity}
             aria-label="Enter city"
             aria-describedby="basic-addon2"
           />
-          <Button variant="outline-info" id="button-addon2">
+          <Button variant="outline-info" id="button-addon2" type="submit">
             Enter
           </Button>
         </InputGroup>
@@ -32,9 +80,9 @@ export default function WeatherCard() {
               <Row className="currentWeatherContainer">
                 <CurrentWeather
                   day="Sunday"
-                  currentTemp="27"
-                  currentLocation="Norwalk"
-                  currentWeatherDesc="Sunny"
+                  currentTemp={weatherConditions.temp}
+                  currentLocation={location}
+                  currentWeatherDesc={weatherConditions.weatherDesc}
                   icon="🌞"
                   currentHumidity="20%"
                   currentWind="8mph"
