b627f0bfe93b20b81851f6b652e08e03ded76170
Ldbshear-jones <ldbshearjones@gmail.com>
2022-06-01 22:59:20 -0400

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="mastfoot mt-auto">
+        <div className="inner">
+          <p>
+            Code at <a href="https://getbootstrap.com/">Github</a>, by{" "}
+            <a href="https://twitter.com/mdo">Louisa</a>.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
